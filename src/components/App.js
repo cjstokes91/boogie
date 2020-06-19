a42ce04765148cd705a924cc956a3e0fdb06d8b3
carl stokes <carlstokes@carls-MBP.lan>
2020-06-19 14:39:43 -0700

    build mern stack

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>List of greetings
+          <li>hi</li>
+          <li>hola</li>
+          <li>bonjour</li>
+          <li>hey</li>
+          <li>howdy</li>
+          <li>wat up</li>
+          <li>morning</li>
+        </ul>
+      </div>
+    )
+  }
+
+}
+
+export default App;
