ba577b636729fd456c39fc287234997cdf8b9ded
carl stokes <carlstokes@carls-MBP.lan>
2020-06-19 13:52:26 -0700

    install mongoose

--- server.js
+++ server.js
@@ -8,3 +8,18 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+//api routes go before "catch all" route 
+// app.use('/api/boogie', boogiesRouter); 
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+    console.log(`Express app listening on port ${port}`)
+});
